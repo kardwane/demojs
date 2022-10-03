a166596fbec1842de92b22dc3bbc5a3c024f35e1
kardwane <48173614+kardwane@users.noreply.github.com>
2022-10-03 13:42:12 +0200

    ajout debut exo part 1

---
+++ assets/js/exo-part1.js
@@ -0,0 +1,20 @@
+//================================ EXO 1 ============================
+/*
+let a = 1
+let b = a + 3
+a = 3
+
+//afficher un résultat
+console.log(`a vaut ${a} b vaut ${b}`) // backtic ` => altGr + 7 (au dessus de u)
+console.log("a vaut " + a + " b vaut " + b) // backtic ` => altGr + 7 (au dessus de u)
+console.log("a vaut ${a} b vaut ${b}") // backtic ` => altGr + 7 (au dessus de u)
+*/
+//================================ EXO 2 ============================
+a = 5 // a vaut 5
+b = 3 // b vaut 3
+c = a + b // 5 + 3 => 8
+a = 2 // a ne vaut plus 5 mais bien 2
+c = b - a // 3 - 2 => 1 
+
+//afficher un résultat
+console.log(`a vaut ${a}, b vaut ${b}, c vaut ${c}`) // backtic ` => altGr + 7 (au dessus de u)

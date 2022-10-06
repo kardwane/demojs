826fbe378122cc6c40e9b85aa746cbb7ba3ce6f1
kardwane <48173614+kardwane@users.noreply.github.com>
2022-10-06 10:06:33 +0200

    update juste prix

---
+++ assets/js/functions.js
@@ -0,0 +1,12 @@
+function doubleValeur(nombre) {
+  nombre = nombre * 2
+  alert(nombre)
+}
+
+// const doubleValue = (id) => {
+//   document.querySelector("#" + id).value
+
+//   alert(nombre * 2)
+// }
+
+// document.querySelector("#arrowBtn").addEventListener("click", doubleValue(nombre))
--- justeprix/assets/js/justeprix.js
+++ justeprix/assets/js/justeprix.js
@@ -1,20 +1,60 @@
 // je déclare toutes mes variables
-const price = document.querySelector("#price") // recupere la balise input "price"
-const submitBtn = document.querySelector("#submitBtn") //je recupere le bouton type submit
-const result = document.querySelector("#result") //je recupere le bouton type submit
-const priceToFind = Math.floor(Math.random() * 100) // prix aléatoire entre 1 et 100
-const giftArray= ["une trotinnette", "un chameau", "une poire à lavement", "un pc portable", "une voiture thermique avec un bidon d'essence vide"]
-const selectedGift = Math.ceil(Math.random() * (giftArray.length - 1)) // prix aléatoire entre 1 et 100
+const price = document.querySelector("#price") // recupere la balise input "#price"
+const submitBtn = document.querySelector("#submitBtn") //je recupere le bouton "#submitBtn"
+const restartBtn = document.querySelector("#restartBtn") //je recupere le bouton "#submitBtn"
+const result = document.querySelector("#result") //je recupere le p #result
+const tryDone = document.querySelector("#tryDone") //je recupere le p #result
 
-let tryNumber = 0
+/*
+je crée un tableau pour stocker tout les cadeaux disponibles
+let monTableau = ["cadeau1", "cadeau2"...] 
+let monTableau2 = [12, 34...] 
+*/
+const giftArray = [
+  "une trotinnette",
+  "un chameau",
+  "une poire à lavement",
+  "un pc portable",
+  "une voiture thermique avec un bidon d'essence vide",
+]
 
-console.log(priceToFind)
+let priceToFind = Math.floor(Math.random() * 100)
+let selectedGift = Math.ceil(Math.random() * (giftArray.length - 1)) // prix aléatoire entre 1 et 100
+let tryNumber = 0 //initialisation
+
+console.log(priceToFind) // cheat code
+
+function start() {
+  /*
+Math.random() => genere un chiffre a virgule compris entre 0 et 1 exemple 0,43235345346546565
+ensuite on multiplie le chiffre par la valeur max attendu (100 dans notre exemple, car le prix max possible est 100)
+mais on obtient toujours un resultat en nombre flotant (a virgule), ex : 23,4578999908908
+enfin il faut arrondir ce chiffre pour le convertir entier (Math.floor())
+*/
+  priceToFind = Math.floor(Math.random() * 100) // prix aléatoire entre 1 et 100
+  /*
+pour choisir mon n° de cadeau, je genere un entier compris entre 0 
+et la taille de mon tableau (tabeau.lenght), cependant je n'oublie pas de faire longueur tableau 
+-1 pour prendre en compte le fait que la 1er case d'un tableau (en js, php..)est la case n°0
+*/
+  selectedGift = Math.ceil(Math.random() * (giftArray.length - 1)) // prix aléatoire entre 1 et 100
+
+  //je créer mon compteur d'essais, en l'initialisant à 0
+  tryNumber = 0 //initialisation
+
+  //je remet mon affichage à 0
+  result.textContent = ""
+  tryDone.textContent = "0 essai"
+
+  console.log(priceToFind) // cheat code
+}
 
 //je lui ajoute une action sur l'evenement "click"
+//addEventListener permet d'ajouter une action sur un evenement
+//ici j'associe une action sur le click de mon bouton
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault() // annule le comportement par défaut du formulaire => empeche la soumisson direct du formulaire
-
-  tryNumber++ // je compte un essai supplémentaire
+  console.log(e)
 
   //recuperer le prix saisie et le comparer a mon prix mystere
   let inputPrice = parseInt(price.value)
@@ -27,9 +67,7 @@ submitBtn.addEventListener("click", function (e) {
   } else {
     if (inputPrice === priceToFind) {
       // j'ajoute un nouveau message à l'historique
-      resultContent =
-        `Vous avez gagné le lot suivant : ${giftArray[selectedGift]}`
-
+      resultContent = `Vous avez gagné le lot suivant : ${giftArray[selectedGift]}`
     } else if (inputPrice < priceToFind) {
       // j'ajoute un nouveau message à l'historique
       resultContent += ", Plus grand!"
@@ -40,4 +78,11 @@ submitBtn.addEventListener("click", function (e) {
     //afficher le nouvel historique
     result.textContent = resultContent
   }
+
+  tryNumber++ // je compte un essai supplémentaire => tryNumber = tryNumber + 1
+  tryDone.textContent = tryNumber + " essais"
+})
+
+restartBtn.addEventListener("click", function () {
+  start()
 })

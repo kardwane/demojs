721c4600ade88a56c4ee6ad0d7a31df0505d3286
kardwane <48173614+kardwane@users.noreply.github.com>
2022-10-17 16:04:01 +0200

    modif sur event et dom

--- assets/js/event.js
+++ assets/js/event.js
@@ -2,19 +2,24 @@ const themeLink = document.querySelector("#theme")
 const pageBody = document.querySelector("body")
 const addLorem = document.querySelector("#addLorem")
 const result = document.querySelector("#result")
+const themeName = document.querySelector(".theme-name")
 
 themeLink.onmouseover = () => {
   if (pageBody.classList.contains("dark-mode")) {
     pageBody.classList.remove("dark-mode")
+    themeName.textContent="Dark mode"
   } else {
     pageBody.classList.add("dark-mode")
+    themeName.textContent="Light mode"
   }
 }
 
-
 addLorem.addEventListener('click', function(){
-  let newP = document.createElement('p')
+  //je défini mon nouvel élément
+  let newP = document.createElement('p') // création en mémoire
   newP.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ipsam vitae nulla facilis possimus molestiae necessitatibus optio ab nesciunt voluptatibus, ratione quasi laborum. Corrupti, beatae odit! Possimus a odio voluptatem."
   newP.id = "newP"+document.querySelectorAll('#result p').length
+  newP.style='color:red;'
+  // une fois prêt, je l'ajoute a mon doument HTML
   result.appendChild(newP)
 })

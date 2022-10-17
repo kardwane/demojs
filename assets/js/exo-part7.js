d371ad879a259a5da6be529f2c4d319d5e13714a
kardwane <48173614+kardwane@users.noreply.github.com>
2022-10-17 13:19:42 +0200

    fin exo part 7

--- assets/js/exo-part7.js
+++ assets/js/exo-part7.js
@@ -1,7 +1,7 @@
 /*
  * La doc MDN sur les tableaux JS : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array
  */
-const result = document.querySelector("#result");
+const result = document.querySelector("#result")
 //! ===============================EXO 1============================== remplir un tableau et dire si ses éléments sont consécutif ou non
 /*
 let value = 1,
@@ -27,11 +27,13 @@ while (value !== 0) {
 result.textContent = (isConsecutif) ? "Consécutif" : "Non consécutif";
 */
 //! ===============================EXO 2============================== trier un tableau avec le tri à bulle et le tri par insertion
-let myArray = [12, 45, 32, 65, 23, 56, 87, 54, 78];
 
-result.innerHTML = `<p>tableau de départ : </p><p>${myArray.toString()}</p>`;
+/*
+let myArray = [12, 45, 32, 65, 23, 56, 87, 54, 78]
 
-// /* 
+result.innerHTML = `<p>tableau de départ : </p><p>${myArray.toString()}</p>`
+
+// 
 //  * version tri par insertion
 //  * explication de ce tri par une danse : https://www.youtube.com/watch?v=EdIKIf9mHk0&list=PLOmdoKois7_FK-ySGwHBkltzB11snW7KQ&index=1
 //  */
@@ -51,35 +53,102 @@ result.innerHTML = `<p>tableau de départ : </p><p>${myArray.toString()}</p>`;
 // 	myArray[j] = currentValue
 // }
 
-/* 
+/*
  * version tri par à bulle
  * explication de ce tri par une danse : https://www.youtube.com/watch?v=semGJAJ7i74&list=PLOmdoKois7_FK-ySGwHBkltzB11snW7KQ&index=2
  */
 
-let isSorted = false; // on pense que le tableau n'est pas trié
-while(!isSorted){	//tant que le tri n'est pas fini, on continue le tri
-	isSorted = true; // on pense que le tableau est trié
-	for(let i=0; i<myArray.length-1; i++ )	{ // on vérifie le tri
-		if(myArray[i]>myArray[i+1]){	// si ma valeur actuelle > a la valeur suivante du tableau, on inverse et on passe le tableau a "non trié"
-			//inversion => cf verre d'eau et de coca a inverser
-			let temp = myArray[i] 
-			myArray[i] = myArray[i+1]
-			myArray[i+1] = temp
-			//tri non fini
-			isSorted = false
-		}
-	}
-}
-
-result.innerHTML += `<p>tableau trié : </p><p>${myArray.toString()}</p>`;
+// let isSorted = false; // on pense que le tableau n'est pas trié
+// while(!isSorted){	//tant que le tri n'est pas fini, on continue le tri
+// 	isSorted = true; // on pense que le tableau est trié
+// 	for(let i=0; i<myArray.length-1; i++ )	{ // on vérifie le tri
+// 		if(myArray[i]>myArray[i+1]){	// si ma valeur actuelle > a la valeur suivante du tableau, on inverse et on passe le tableau a "non trié"
+// 			//inversion => cf verre d'eau et de coca a inverser
+// 			let temp = myArray[i]
+// 			myArray[i] = myArray[i+1]
+// 			myArray[i+1] = temp
+// 			//tri non fini
+// 			isSorted = false
+// 		}
+// 	}
+// }
 
 /*
-* la playlist des danse de tri : 	
-* https://www.youtube.com/playlist?list=PLOmdoKois7_FK-ySGwHBkltzB11snW7KQ
+ * version sort() de js : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+ */
+
+// myArray.sort((a, b) => a - b) //croissant
+// // myArray.sort((a, b) => b - a) //décroissant
+
+// result.innerHTML += `<p>tableau trié : </p><p>${myArray.toString()}</p>`
+
+/*
+ * la playlist des danse de tri :
+ * https://www.youtube.com/playlist?list=PLOmdoKois7_FK-ySGwHBkltzB11snW7KQ
+ */
+
+//! ===============================EXO 3============================== remplir un tableau puis inverser les valeurs, les 1er seront les derniers
+/*
+let myArray = [12, 45, 32, 65, 23, 56, 87, 54, 78]
+result.innerHTML = `<p>tableau de départ : </p><p>${myArray.toString()}</p>`
+ 
+//version manuel
+// let arrayResult = []
+// for (let i = 0; i < myArray.length; i++) {
+//   arrayResult[i] = myArray[myArray.length - i - 1]
+// }
+// result.innerHTML += `<p>tableau inversé : </p><p>${arrayResult.toString()}</p>`
+
+//version js
+result.innerHTML += `<p>tableau inversé : </p><p>${myArray.reverse()}</p>`
 */
 
-//! ===============================EXO 3============================== remplir un tableau de neuf saisie utilisateur
+//! ===============================EXO 4============================== supprimer une case de tableau
+/*
+let myArray = [12, 8, 4, 45, 64, 9, 2]
 
-//! ===============================EXO 4============================== améliorer l'algo donner par l'enoncé
+result.innerHTML = `<p>tableau de départ : </p><p>${myArray.toString()}</p>`
 
-//! ===============================EXO 5============================== améliorer l'algo donner par l'enoncé
+const nb = parseInt(
+  prompt("Quel case souhaitez-vous supprimer (1ere case = 1)?")
+)
+myArray.splice(nb - 1, 1)
+
+result.innerHTML += `<p>tableau après suppression : </p><p>${myArray.toString()}</p>`
+*/
+
+//! ===============================EXO 5============================== chercher un mot dans un tableau
+
+let myArray = [
+  "bonjour",
+  "formation",
+  "développeur",
+  "acajoux",
+  "loutre",
+  "route",
+  "avion",
+]
+
+const recherche = prompt("Quel mot chechez vous ?")
+
+//version foreach
+let isFind = false
+
+myArray.forEach((value) => {
+  if (value === recherche) isFind = true
+})
+
+if (isFind) {
+  console.log(`${recherche} trouvé dans ${myArray.toString()}`)
+} else {
+  console.log(`${recherche} non trouvé dans ${myArray.toString()}`)
+}
+
+//version JS
+// const resultat = myArray.find((mot) => mot === recherche)
+
+// if (resultat !== undefined) {
+//   console.log(`${recherche} trouvé dans ${myArray.toString()}`)
+// } else {
+//   console.log(`${recherche} non trouvé dans ${myArray.toString()}`)
+// }

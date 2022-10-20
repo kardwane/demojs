cca5a24766c94ffff0e9f0b320ad712f006a8e3c
Kardwane <kardwane@gmail.com>
2022-10-20 20:50:42 +0200

    update todolist en cours (modal ok, font et animation a ajouter

--- todolist/assets/js/todolist.js
+++ todolist/assets/js/todolist.js
@@ -1,76 +1,109 @@
 const addBtn = document.querySelector("#addBtn")
 const todolist = document.querySelector("#todolist")
 const todoDesc = document.querySelector("#todoDesc")
-const todoCounter = 1
+const modal = document.querySelector("#appModal");
+const closeModalSpan = document.querySelector("#closeModalSpan");
+const closeModalBtn = document.querySelector("#closeModalBtn");
+
 
 let todoOldValue = ""
 let todos = ["tâche exemple"]
 
 addBtn.onclick = (e) => {
-  e.preventDefault()
+    e.preventDefault()
 
-  //j'ajoute ma todo a mon tableau de todos
-  todos.push(todoDesc.value)
-  readTodos()
+    //j'ajoute ma to-do a mon tableau de todos
+    todos.push(todoDesc.value)
+    readTodos()
 }
 
 // permet d'afficher toutes les taches en attente
 const readTodos = () => {
-  todolist.innerHTML = ""
-  todos.forEach((value, index) => {
-    displayTodo(value, index)
-  })
+    todolist.innerHTML = ""
+    // todos.forEach((value, index) => {
+    //   displayTodo(value, index)
+    // })
+    todos.map((value, index) => {
+        displayTodo(value, index)
+        return value;
+    })
 }
 
-//se concentre sur l'afficahge d'une seule todo
+//se concentre sur l'affichage d'une seule to-do
 const displayTodo = (todoDesc, todoId) => {
-  const todo = document.createElement("article")
-  todo.id = "todo" + todoId
-  todo.innerHTML = `
-  <div class="todo-description">${todoDesc}</div>
+    const todo = document.createElement("article")
+    todo.id = "todo" + todoId
+    todo.innerHTML = `
+  <div id="todoDescription">${todoDesc}</div>
   <div>
-      <a href="#" onclick="createUpdateTodo(${todoId})"><img class="action-icon" src="./assets/img/edit.png"></a>
-      <a href="#" onclick="strikeTodo(${todoId})"><img class="action-icon" src="./assets/img/validate.png"></a>
-      <a href="#" onclick="removeTodo(${todoId})"><img class="action-icon" src="./assets/img/delete.png"></a>
+      <a href="#" onclick="createUpdateTodo(${todoId})"><img class="action-icon" src="./assets/img/edit.png" alt="update"></a>
+      <a href="#" onclick="strikeTodo(${todoId})"><img class="action-icon" src="./assets/img/validate.png" alt="strike"></a>
+      <a href="#" onclick="popupModal(${todoId})"><img class="action-icon" src="./assets/img/delete.png" alt="remove"></a>
       </div>
       `
-  todolist.appendChild(todo)
+    todolist.appendChild(todo)
+    document.querySelector("#todoDescription").onclick=()=>createUpdateTodo(todoId)   //active la maj sur le click
 }
 
-//affiche la zone de maj de la todo
+//affiche la zone de maj de la to-do
 const createUpdateTodo = (id) => {
-  const todo = document.querySelector("#todo" + id)
-  todoOldValue = todo.childNodes[1].textContent
-  todo.innerHTML = `
+    const todo = document.querySelector("#todo" + id)
+    todoOldValue = todo.childNodes[1].textContent
+    todo.innerHTML = `
   <label for="todoDesc">
   Description de la Tâche
-  <input type="text" name="todoDescUpdate" id="todoDescUpdate" />
   </label>
+  <input type="text" id="todoDescUpdate" />
     <a href="#" onclick="doUpdateTodo(${id})"><img class="action-icon" src="./assets/img/validate.png"></a>
   `
+    // gère la maj sur la touche "entrer" du clavier
+    document.querySelector("#todoDescUpdate").onkeyup=(e)=>{
+        if (e.key === "Enter") doUpdateTodo(id)
+    }
 }
 
 const doUpdateTodo = (id) => {
-  const inputValue = document.querySelector("#todoDescUpdate").value
-  const todoNewValue = inputValue === "" ? todoOldValue : inputValue
-
-  todos[id] = todoNewValue
-
-  readTodos()
+    const inputValue = document.querySelector("#todoDescUpdate").value
+    todos[id] = inputValue === "" ? todoOldValue : inputValue
+    readTodos()
 }
 
 const strikeTodo = (id) => {
-  document
-    .querySelector("#todo" + id)
-    .childNodes[1].classList.toggle("task-done")
+    document
+        .querySelector("#todo" + id)    // je selectionne la tâche concernée
+        .childNodes[1].classList.toggle("task-done")    // pour lui faire inverser la classe task-done
 }
 
 const removeTodo = (id) => {
-  //on supprime la todo du tableau
-  todos.splice(id, 1)
-  //on raffraichi l'affichage de la liste
-  readTodos()
+    //on supprime la to-do du tableau
+    todos.splice(id, 1)
+    closeModal() // ferme la modal
+    //on rafraichit l'affichage de la liste
+    readTodos()
 }
+
+const popupModal = (id) => {
+    // ouverture de la modal pour avoir la confirmation de suppression
+    modal.style.display = "block";
+    document.querySelector('#deleteConfirm').onclick = () => removeTodo(id)
+}
+
+const closeModal = () => {
+    modal.style.display = "none";
+}
+
+//========== section modal ============
+
+// on gère la fermeture de la modal par le span ou le bouton "non"
+closeModalBtn.onclick = () => closeModal()
+closeModalSpan.onclick = () => closeModal()
+
+// on ferme modal si l'utilisateur click en dehors de la fenêtre
+window.onclick = function (event) {
+    if (event.target === modal) modal.style.display = "none";
+}
+
+//========== end section ==============
 
 // on affiche la todolist par défaut
 readTodos()

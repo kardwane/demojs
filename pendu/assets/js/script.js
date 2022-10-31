973c057b16a60c70a686a4370f88a9f26c76dd89
kardwane <48173614+kardwane@users.noreply.github.com>
2022-10-31 09:09:56 +0100

    update todolist (theme) + ajout exemple pendu

---
+++ pendu/assets/js/script.js
@@ -0,0 +1,144 @@
+const words = ["tonneaux", "gomme", "presidentielle", "vietnam", "javascript", "jeux", "aragorn", "xylophone"];
+const endgame = 7;
+const maskedWord = document.querySelector("#maskedWord");
+const vKeyboard = document.querySelector("#vKeyboard");
+const hangmanStep = document.querySelector("#hangmanStep");
+const letters = "azertyuiopqsdfghjklmwxcvbn";
+
+let randomIndex, wordToFind, fail;
+
+const init = () => {
+    randomIndex = Math.floor(Math.random() * (words.length - 1));
+    wordToFind = words[randomIndex];
+    fail = 0;
+    maskedWord.innerHTML = createMaskedWord();
+    createVirtualKeyboard();
+    document.querySelector(".restart").textContent = "";
+    hangmanStep.style.backgroundPosition = "2% 60%";
+};
+
+const updateMaskedWord = (wordToShow, letter) => {
+    let newMaskedWord = "";
+    for (let i = 0; i < wordToFind.length; i++) {
+        if (letter === wordToFind[i]) {
+            newMaskedWord += letter;
+        } else {
+            newMaskedWord += wordToShow[i];
+        }
+    }
+    return newMaskedWord;
+};
+
+const createMaskedWord = () => {
+    let newMaskedWord = "";
+    for (let i = 0; i < wordToFind.length; i++) {
+        newMaskedWord += "*";
+    }
+    return newMaskedWord;
+};
+
+const createEndMessage = (isWinner) => {
+    let cssClass = "endmessagev";
+    const newP = document.createElement("p");
+    newP.innerHTML = 'Vous avez gagné <img src="https://media2.giphy.com/media/lnyPptAfGwHeTdoQDk/giphy.gif?cid=ecf05e47i6s88fufl369lmxlxixeqtawrobjyz2e2rouhfpe&rid=giphy.gif&ct=g"/>';
+    if (!isWinner) {
+        cssClass = "endmessaged";
+        newP.innerHTML =
+            "Vous avez perdu, le mot à trouver était : " + wordToFind +
+            ' <img src="https://media4.giphy.com/media/QmGNyXP5MRFgeFERPd/giphy.gif?cid=ecf05e47noe95fuwwg8v1ifpx1eve59xlwoapg1zm4k5uutr&rid=giphy.gif&ct=g"/>';
+    }
+    newP.classList.add(cssClass);
+    vKeyboard.innerHTML = "";
+    vKeyboard.appendChild(newP);
+};
+
+const showRestartButton = (result) => {
+    const restart = document.createElement("button");
+    restart.innerHTML = "Recommencer";
+    restart.onclick = () => {
+        init();
+    };
+    document.querySelector(".restart").appendChild(restart);
+    createEndMessage(result);
+};
+
+const winTurn = (letter) => {
+    let newMaskedWord = updateMaskedWord(maskedWord.innerHTML, letter);
+    maskedWord.innerHTML = newMaskedWord;
+    if (newMaskedWord === wordToFind) {
+        const victoire = new Audio(
+            "./assets/sound/mixkit-animated-small-group-applause-523.wav"
+        );
+        victoire.play();
+        showRestartButton(true);
+    }
+};
+
+const looseTurn = () => {
+    fail++;
+    document.querySelector("#spanll").textContent = (endgame - fail) + " coup(s) restant(s)";
+    switch (fail) {
+        case 1:
+            hangmanStep.style.backgroundPosition = "16% 60%";
+            break;
+        case 2:
+            hangmanStep.style.backgroundPosition = "29% 60%";
+            break;
+        case 3:
+            hangmanStep.style.backgroundPosition = "43% 60%";
+            break;
+        case 4:
+            hangmanStep.style.backgroundPosition = "57% 60%";
+            break;
+        case 5:
+            hangmanStep.style.backgroundPosition = "70% 60%";
+            break;
+        case 6:
+            hangmanStep.style.backgroundPosition = "84% 60%";
+            break;
+        case 7:
+            hangmanStep.style.backgroundPosition = "99% 60%";
+            break;
+        default:
+            hangmanStep.style.backgroundPosition = "2% 60%";
+    }
+    if (fail === endgame) {
+        const defaite = new Audio(
+            "./assets/sound/mixkit-player-losing-or-failing-2042.wav"
+        );
+        defaite.play();
+        showRestartButton(false);
+    }
+};
+
+const checkLetter = (letter) => {
+    document.querySelector("#btn" + letter).disabled = true;
+    if (wordToFind.indexOf(letter) !== -1) {
+        winTurn(letter);
+    } else {
+        looseTurn();
+    }
+    return true;
+};
+
+const createVirtualKeyboard = () => {
+    vKeyboard.innerHTML = "";
+    for (let i = 0; i < letters.length; i++) {
+        let newBtnLetter = document.createElement("button");
+        newBtnLetter.innerHTML = letters[i].toUpperCase();
+        newBtnLetter.id = "btn" + letters[i];
+        newBtnLetter.classList.add("btn-letter");
+        newBtnLetter.onclick = () => {
+            checkLetter(letters[i]);
+        };
+        vKeyboard.appendChild(newBtnLetter);
+    }
+};
+
+window.document.body.onkeyup = (e) => {
+    if (letters.match(e.key)) {
+        if (!document.querySelector("#btn" + e.key).disabled) checkLetter(e.key);
+    }
+};
+
+init();
--- todolist/assets/js/todolist.js
+++ todolist/assets/js/todolist.js
@@ -1,17 +1,31 @@
-const addBtn = document.querySelector("#addBtn")
-const todolist = document.querySelector("#todolist")
-const todoDesc = document.querySelector("#todoDesc")
-const todoCounter = 1
+const addBtn = document.querySelector("#addBtn") // bouton ajout
+const todolist = document.querySelector("#todolist") //le conteneur de la liste
+const todoDesc = document.querySelector("#todoDesc") // l'input d'ajout de tache
+const modal = document.getElementById("appModal") // la modal
+const span = document.getElementsByClassName("close")[0] // le bouton de fermeture de la modal (X)
+const modalYesBtn = document.querySelector("#modalYesBtn")
 
-let todoOldValue = ""
-let todos = ["tâche exemple"]
+let todoOldValue = "" // permet de sauvegarder la valeur de la tache avant affichage du form de modif
+let todos = ["tâche exemple"] // création du tableau de todo
 
-addBtn.onclick = (e) => {
+// action d'ajout de todo
+const addTodo = (e) => {
   e.preventDefault()
 
   //j'ajoute ma todo a mon tableau de todos
   todos.push(todoDesc.value)
   readTodos()
+  todoDesc.value = ""
+  todoDesc.focus()
+}
+
+//liaison de l'action
+addBtn.onclick = (e) => addTodo(e) // sur le click du bouton
+todoDesc.onkeyup = (e) => {
+  // sur la touche entrer du clavier
+  if (e.key === "Enter") {
+    addTodo(e)
+  }
 }
 
 // permet d'afficher toutes les taches en attente
@@ -22,18 +36,18 @@ const readTodos = () => {
   })
 }
 
-//se concentre sur l'afficahge d'une seule todo
+//se concentre sur l'affichage d'une seule todo
 const displayTodo = (todoDesc, todoId) => {
   const todo = document.createElement("article")
   todo.id = "todo" + todoId
   todo.innerHTML = `
   <div class="todo-description">${todoDesc}</div>
-  <div>
-      <a href="#" onclick="createUpdateTodo(${todoId})"><img class="action-icon" src="./assets/img/edit.png"></a>
-      <a href="#" onclick="strikeTodo(${todoId})"><img class="action-icon" src="./assets/img/validate.png"></a>
-      <a href="#" onclick="removeTodo(${todoId})"><img class="action-icon" src="./assets/img/delete.png"></a>
-      </div>
-      `
+  <div class="action-container">
+    <i class="fi fi-rs-edit action-icon" onclick="createUpdateTodo(${todoId})"></i>
+    <i class="fi fi-rs-check action-icon" onclick="strikeTodo(${todoId})"></i>
+    <i class="fi fi-rs-trash action-icon" onclick="showModal(${todoId})"></i>
+  </div>`
+
   todolist.appendChild(todo)
 }
 
@@ -42,35 +56,60 @@ const createUpdateTodo = (id) => {
   const todo = document.querySelector("#todo" + id)
   todoOldValue = todo.childNodes[1].textContent
   todo.innerHTML = `
-  <label for="todoDesc">
-  Description de la Tâche
-  <input type="text" name="todoDescUpdate" id="todoDescUpdate" />
-  </label>
-    <a href="#" onclick="doUpdateTodo(${id})"><img class="action-icon" src="./assets/img/validate.png"></a>
+  <input type="text" name="todoDescUpdate" id="todoDescUpdate" value="${todoOldValue}"/>
+  <a href="#" onclick="doUpdateTodo(${id})" class="action-container"><i class="fi fi-rs-check action-icon"</a></i>
   `
 }
 
+// la mise a jour
 const doUpdateTodo = (id) => {
+  //on recuper la nouvelle valeur saisie
   const inputValue = document.querySelector("#todoDescUpdate").value
+  // si elle est vide alors on ne change rien
   const todoNewValue = inputValue === "" ? todoOldValue : inputValue
-
+  // maj dans le tableau
   todos[id] = todoNewValue
-
+  // refresh des todos
   readTodos()
 }
 
 const strikeTodo = (id) => {
+  //on recupere la todo choisi
   document
     .querySelector("#todo" + id)
-    .childNodes[1].classList.toggle("task-done")
+    .childNodes[1].classList.toggle("task-done") // on lui toggle la classe task-done
 }
 
+//suppression todo
 const removeTodo = (id) => {
-  //on supprime la todo du tableau
-  todos.splice(id, 1)
-  //on raffraichi l'affichage de la liste
-  readTodos()
+  todos.splice(id, 1) //on supprime la todo du tableau
+  closeModal() //on ferme la modal
+  readTodos() //on raffraichi l'affichage de la liste
 }
 
-// on affiche la todolist par défaut
-readTodos()
+//afifchage de la modale
+const showModal = (id) => {
+  modal.style.display = "block"  // grace a un display block
+  modalYesBtn.onclick = (id) => removeTodo(id)  // on met la bonne action sur le bouton "oui"
+}
+
+//fermture todo
+const closeModal = () => {
+  modal.style.display = "none"
+}
+
+readTodos() // on affiche la todolist par défaut
+
+// ======================= modal ===================
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  closeModal()
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    closeModal()
+  }
+}
